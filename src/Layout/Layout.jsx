9c51a700f21c821f70d664b62ef2f0025eafcf8f
Mahmoud Saaed Romeh <123378237+Engromeh@users.noreply.github.com>
2025-02-12 01:05:45 +0200

    layout done

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -2,19 +2,24 @@ import React from "react";
 import Sidbar from "../Pages/Sidbar/Sidbar";
 import Navbar from "../Pages/Navbar/Navbar";
 import Statistics from "../Pages/Statistics/Statistics";
+import UserInfo from "../Pages/UserInfo/UserInfo";
 
 const Layout = () => {
   return (
     <div className="grid grid-cols-12 ">
-      <aside className="col-span-3">
+      <aside className="col-span-2">
         <Sidbar />
       </aside>
 
-      <div className="col-span-6 ">
+      <div className="col-span-7 ">
         <Navbar />
         <Statistics />
       </div>
-      <aside className="col-span-3">{/* <UserInfo /> */}</aside>
+
+      <aside className="col-span-3" style={{marginLeft:"5%"}}>
+
+    <UserInfo /> 
+      </aside>
     </div>
   );
 };
